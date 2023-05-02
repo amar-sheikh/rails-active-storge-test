cf03fc845541c5082d3ba163424f044fda171334
umar <umar.m.sheikh@gmail.com>
2023-05-02 15:41:25 +0500

    Validate image size less than 200kb

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -7,7 +7,7 @@ class Photo < ApplicationRecord
 
   validates :image, presence: true
   validates :caption, presence: true, length: {minimun: 1, maximum: 100}
-  validate :valid_image_type
+  validate :valid_image_type, :valid_image_size
 
   private
 
@@ -16,6 +16,14 @@ class Photo < ApplicationRecord
       true
     else
       self.errors.add :image, "must be in jpg or png format"
+    end
+  end
+
+  def valid_image_size
+    if self.image.byte_size < 200_000
+      true
+    else
+      self.errors.add :image, "size should be less than 200kb"
     end
   end
 
